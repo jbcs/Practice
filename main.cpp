d1399be21459d544e718e1137947d89e0bc41d84
jbcs <jbcomscore@gmail.com>
2013-12-10 15:37:38 -0500

    Added in the name to main.cpp
    Needed additional information for main.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ using namespce std;
 int main(int arg, char **argc)
 {
 	int iAge = 0;
+	string strName;
 
 	cout << "Please enter your age: ";
 	cin >> iAge;
+
+	cout << "Please enter your name: ";
+	cin >> strName;
+
 
 	cout << "Got it going on!" << endl;
 	return EXIT_SUCCESS;
