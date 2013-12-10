f474d819c8a520620fe19b3e0050e65565e5d2ca
jbehrbaum <jbehrbaum@office.comscore.com>
2013-12-10 15:43:01 -0500

    Needed to add in the last name.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@ using namespce std;
 int main(int arg, char **argc)
 {
 	int iAge = 0;
-	string strName;
+	string strFName;
+	string strLName;
 
 	cout << "Please enter your age: ";
 	cin >> iAge;
 
-	cout << "Please enter your name: ";
-	cin >> strName;
+	cout << "Please enter your first name: ";
+	cin >> strFName;
 
+	cout << "Please enter your last name: ";
+	cin >> strLName;
 
 	cout << "Got it going on!" << endl;
 	return EXIT_SUCCESS;
