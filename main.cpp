c77073b2911dded4f82009b0091425ee04b84912
jbehrbaum <jbehrbaum@office.comscore.com>
2013-12-10 13:57:23 -0500

    This is the second commit for main.cpp There still has been no push

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ using namespce std;
 
 int main(int arg, char **argc)
 {
+	int iAge = 0;
+
+	cout << "Please enter your age: ";
+	cin >> iAge;
+
 	cout << "Got it going on!" << endl;
 	return EXIT_SUCCESS;
 }
