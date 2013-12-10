4d38d2dc201b3b7057253216988f2a3dceac5ced
jbehrbaum <jbehrbaum@office.comscore.com>
2013-12-10 13:47:05 -0500

    This is my initial commit from another computer - my MAC. I'll do an update in a minute so I can checkout how main.cpp's changes are handled.

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is an initial file for a readme.  This is my 
+readme and is basic.
+
+I almost forgot - I need to add some more usful information.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespce std;
+
+int main(int arg, char **argc)
+{
+	cout << "Got it going on!" << endl;
+	return EXIT_SUCCESS;
+}
